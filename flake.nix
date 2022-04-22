b9bb133b154643a65e24f23c0aa018008633b6e9
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-04-22 21:01:19 +0200

    switch to mach-nix for python

--- flake.nix
+++ flake.nix
@@ -1,15 +1,28 @@
 {
   description = "pyoperon";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nur.url = "github:nix-community/NUR";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nur.url = "github:nix-community/NUR";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
 
-  inputs.operon.url = "github:heal-research/operon/cpp20";
-  inputs.pratt-parser.url = "github:foolnotion/pratt-parser-calculator?rev=a15528b1a9acfe6adefeb41334bce43bdb8d578c";
-  inputs.vstat.url = "github:heal-research/vstat/cpp20-eve";
+    operon.url = "github:heal-research/operon/cpp20";
+    pratt-parser.url = "github:foolnotion/pratt-parser-calculator?rev=a15528b1a9acfe6adefeb41334bce43bdb8d578c";
+    vstat.url = "github:heal-research/vstat/cpp20-eve";
+    pypi-deps-db.url = "github:DavHau/pypi-deps-db";
 
-  outputs = { self, flake-utils, nixpkgs, nur, operon, pratt-parser, vstat }:
+    mach-nix = {
+      url = "github:DavHau/mach-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        pypi-deps-db.follows = "pypi-deps-db";
+      };
+    };
+  };
+
+  outputs =
+    { self, flake-utils, mach-nix, nixpkgs, nur, operon, pratt-parser, pypi-deps-db, vstat }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -17,8 +30,23 @@
           overlays = [ nur.overlay ];
         };
         repo = pkgs.nur.repos.foolnotion;
+        mach = mach-nix.lib.${system};
 
-        python = pkgs.python39.override { stdenv = pkgs.gcc11Stdenv; };
+        python-env = mach.mkPython {
+          requirements = ''
+            eli5
+            joblib
+            numpy
+            optuna
+            pandas
+            pmlb
+            pyyaml
+            scikit-learn
+            sympy
+          '';
+
+          ignoreDataOutdated = true;
+        };
       in rec {
         defaultPackage = pkgs.gcc11Stdenv.mkDerivation {
           name = "pyoperon";
@@ -26,46 +54,30 @@
 
           cmakeFlags = [
             "-DCMAKE_BUILD_TYPE=Release"
-            "-DCMAKE_CXX_FLAGS=${if pkgs.targetPlatform.isx86_64 then "-march=haswell" else ""}"
+            "-DCMAKE_CXX_FLAGS=${
+              if pkgs.targetPlatform.isx86_64 then "-march=haswell" else ""
+            }"
           ];
 
           nativeBuildInputs = with pkgs; [
             cmake
-            python
+            pkg-config
+            python-env.python
+            python-env.python.pkgs.pybind11
           ];
 
           buildInputs = with pkgs; [
-            # python environment for bindings and scripting
-            (python.withPackages (ps:
-              with ps; [
-#                jupyterlab
-                numpy
-                pandas
-                pybind11
-                requests
-                scikit-learn
-                seaborn
-              ]))
-            # Project dependencies and utils for profiling and debugging
             eigen
             fmt
             openlibm
-            pkg-config
+            # python environment for bindings and scripting
+            python-env
             # flakes
             operon.defaultPackage.${system}
             pratt-parser.defaultPackage.${system}
             vstat.defaultPackage.${system}
-            # Some dependencies are provided by a NUR repo
-            repo.aria-csv
-            repo.eve
             repo.fast_float
             repo.robin-hood-hashing
-            repo.taskflow
-            repo.vectorclass
-            repo.xxhash
-            # Needed for the example
-            repo.eli5
-            repo.pmlb
           ];
         };
 
@@ -74,11 +86,11 @@
           hardeningDisable = [ "all" ];
           impureUseNativeOptimizations = true;
           nativeBuildInputs = defaultPackage.nativeBuildInputs;
-          buildInputs = defaultPackage.buildInputs ++ (with pkgs; [ gdb valgrind ]);
+          buildInputs = defaultPackage.buildInputs
+            ++ (with pkgs; [ gdb valgrind ]);
 
           shellHook = ''
             PYTHONPATH=$PYTHONPATH:${defaultPackage.out}
-            LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.gcc11Stdenv.cc.cc.lib ]}:$CMAKE_LIBRARY_PATH;
           '';
         };
       });
