83b2ec6429af386df7e5f1c2ef75cb73feaec7ba
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-03-31 13:07:43 +0200

    update CI and bump version to 0.3.6

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 import os, sys, subprocess
 from skbuild import setup  # This line replaces 'from setuptools import setup'
 
-if 'sdist' in sys.argv:
-    cwd = os.getcwd()
-    result = subprocess.call('./script/dependencies.sh', shell=True)
-    if result != 0:
-        exit(1)
-    os.chdir(cwd)
-
 setup(
     name="pyoperon",
-    version="0.3.5",
+    version="0.3.6",
     description="python bindings for the operon library",
     author='Bogdan Burlacu',
     license="MIT",
