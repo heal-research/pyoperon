aaf7ecf2b2f0adcd2187b790c09004388507dcbe
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-12-04 22:52:10 +0100

    fix setup.py failure due to deprecations in latest setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os, sys, subprocess
 from skbuild import setup  # This line replaces 'from setuptools import setup'
 
 setup(
@@ -6,7 +5,6 @@ setup(
     version="0.3.6",
     description="python bindings for the operon library",
     author='Bogdan Burlacu',
-    license="MIT",
     packages=['pyoperon'],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
