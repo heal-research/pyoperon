1a40ec6e32628628ce09b499c2d9ce12e7449411
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-05-30 23:30:30 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -1,102 +1,101 @@
 {
-  description = "pyoperon";
+  description = "PyOperon development environment";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     foolnotion.url = "github:foolnotion/nur-pkg";
-    nixpkgs.url = "github:nixos/nixpkgs/master";
-    pratt-parser.url = "github:foolnotion/pratt-parser-calculator";
     lbfgs.url = "github:foolnotion/lbfgs";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    operon.url = "github:heal-research/operon";
+    pratt-parser.url = "github:foolnotion/pratt-parser-calculator";
+    vstat.url = "github:heal-research/vstat";
 
     foolnotion.inputs.nixpkgs.follows = "nixpkgs";
     lbfgs.inputs.nixpkgs.follows = "nixpkgs";
+    operon.inputs.nixpkgs.follows = "nixpkgs";
     pratt-parser.inputs.nixpkgs.follows = "nixpkgs";
+    vstat.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, foolnotion, pratt-parser, lbfgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ foolnotion.overlay ];
-        };
-        enableShared = false;
-        stdenv_ = pkgs.llvmPackages_17.stdenv;
-        python_ = pkgs.python311;
+  outputs = inputs@{ self, flake-parts, nixpkgs, foolnotion, pratt-parser, lbfgs, vstat, operon }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
 
-        operon = pkgs.callPackage ./nix/operon {
-          enableShared = enableShared;
-          vstat = pkgs.callPackage ./nix/vstat { };
-          lbfgs = lbfgs.packages.${system}.default;
-          stdenv = stdenv_;
-        };
-
-        pyoperon = stdenv_.mkDerivation {
-          name = "pyoperon";
-          src = self;
-
-          cmakeFlags = [
-            "-DCMAKE_CXX_FLAGS=${if pkgs.hostPlatform.isx86_64 then "-g" else ""}"
-          ];
-
-          cmakeBuildType = "Debug";
-
-          dontStrip = true;
-
-          nativeBuildInputs = with pkgs; [
-            cmake
-            ninja
-            pkg-config
-            python_
-            python_.pkgs.pybind11
-          ];
-
-          buildInputs = with pkgs; [
-            python_.pkgs.setuptools
-            python_.pkgs.wheel
-            python_.pkgs.requests
-            operon
-          ] ++ operon.buildInputs;
-        };
-      in
-      rec {
-        packages = {
-          default = pyoperon;
-          pyoperon-generic = pyoperon.overrideAttrs (old: {
-            cmakeFlags = [
-              "-DCMAKE_BUILD_TYPE=Release"
-              "-DCMAKE_CXX_FLAGS=${
-                if pkgs.hostPlatform.isx86_64 then "-march=x86-64" else ""
-              }"
+      perSystem = { pkgs, system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              foolnotion.overlay
+              (final: prev: {
+                vstat = vstat.packages.${system}.default;
+              })
             ];
-          });
-          pyoperon-debug = pyoperon.overrideAttrs
-            (old: { cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Debug" ]; });
-        };
+          };
+          enableShared = false;
+          stdenv_ = pkgs.llvmPackages_18.stdenv;
+          python_ = pkgs.python311;
+          operon_ = if enableShared then operon.packages.${system}.library else operon.packages.${system}.library-static;
 
-        devShells.default = stdenv_.mkDerivation {
-          name = "pyoperon-dev";
-          nativeBuildInputs = pyoperon.nativeBuildInputs;
-          buildInputs = pyoperon.buildInputs;
-          shellHook = ''
-            $SHELL
-          '';
-        };
+          pyoperon = stdenv_.mkDerivation {
+            name = "pyoperon";
+            src = self;
 
-        devShells.pyenv = stdenv_.mkDerivation {
-          name = "pyoperon-dev";
-          nativeBuildInputs = pyoperon.nativeBuildInputs;
-          buildInputs = pyoperon.buildInputs ++ (with pkgs; [ gdb valgrind gcc13 ])
-                          ++ (with python_.pkgs; [ scikit-build ] ) # cmake integration and release preparation
-                          ++ (with python_.pkgs; [ numpy scikit-learn pandas ipdb sympy requests matplotlib optuna jax jaxlib-bin ])
-                          ++ (with pkgs; [ (pmlb.override { pythonPackages = python_.pkgs; }) ]);
-          shellHook = ''
-            $SHELL
-          '';
-        };
+            cmakeFlags = [
+              "--preset ${if pkgs.stdenv.hostPlatform.isx86_64 then "build-linux" else "build-osx"}"
+            ];
+            cmakeBuildType = "Debug";
+            dontStrip = true;
 
-        # backwards compatibility
-        defaultPackage = packages.default;
-        defaultShell = devShells.default;
-      });
+            nativeBuildInputs = with pkgs; [
+              cmake
+              ninja
+              pkg-config
+              python_
+              python_.pkgs.pybind11
+            ];
+
+            buildInputs = with pkgs; [
+              (python_.withPackages (ps: with ps; [ setuptools wheel requests ]))
+              operon_
+            ] ++ operon_.buildInputs;
+          };
+        in
+        rec {
+          packages = {
+            default = pyoperon;
+            pyoperon-generic = pyoperon.overrideAttrs (old: {
+              cmakeFlags = [
+                "-DCMAKE_BUILD_TYPE=Release"
+                "-DCMAKE_CXX_FLAGS=${
+                  if pkgs.hostPlatform.isx86_64 then "-march=x86-64" else ""
+                }"
+              ];
+            });
+            pyoperon-debug = pyoperon.overrideAttrs
+              (old: { cmakeBuildType = "Debug"; });
+          };
+
+          devShells.default = stdenv_.mkDerivation {
+            name = "pyoperon-dev";
+            nativeBuildInputs = pyoperon.nativeBuildInputs;
+            buildInputs = pyoperon.buildInputs;
+            shellHook = ''
+              $SHELL
+            '';
+          };
+
+          devShells.pyenv = stdenv_.mkDerivation {
+            name = "pyoperon-dev";
+            nativeBuildInputs = pyoperon.nativeBuildInputs;
+            buildInputs = pyoperon.buildInputs ++ (with pkgs; [ gdb valgrind gcc13 pkgs.vstat ])
+              ++ (with python_.pkgs; [ scikit-build ]) # cmake integration and release preparation
+              ++ (with python_.pkgs; [ numpy scikit-learn pandas pyarrow ipdb sympy requests matplotlib optuna jax jaxlib-bin torch ])
+              ++ (with pkgs; [ (pmlb.override { pythonPackages = python_.pkgs; }) ]);
+            shellHook = ''
+              $SHELL
+            '';
+          };
+        };
+    };
 }
