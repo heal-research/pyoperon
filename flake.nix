e4f6990273f639996a824cc52c24428e1984a4e5
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-02-28 21:59:04 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -17,11 +17,25 @@
     vstat.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, foolnotion, pratt-parser, lbfgs, vstat, operon }:
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      nixpkgs,
+      foolnotion,
+      pratt-parser,
+      lbfgs,
+      vstat,
+      operon,
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
 
-      perSystem = { pkgs, system, ... }:
+      perSystem =
+        { pkgs, system, ... }:
         let
           pkgs = import nixpkgs {
             inherit system;
@@ -35,7 +49,11 @@
           enableShared = false;
           stdenv_ = pkgs.llvmPackages_latest.stdenv;
           python_ = pkgs.python3;
-          operon_ = if enableShared then operon.packages.${system}.library else operon.packages.${system}.library-static;
+          operon_ =
+            if enableShared then
+              operon.packages.${system}.library
+            else
+              operon.packages.${system}.library-static;
 
           pyoperon = stdenv_.mkDerivation {
             name = "pyoperon";
@@ -54,10 +72,22 @@
               python_.pkgs.pybind11
             ];
 
-            buildInputs = with pkgs; [
-              (python_.withPackages (ps: with ps; [ setuptools wheel requests nanobind ]))
-              (operon_.overrideAttrs(old: { cmakeFlags = old.cmakeFlags ++ [ "-DUSE_SINGLE_PRECISION=1" ]; }))
-            ] ++ operon_.buildInputs;
+            buildInputs =
+              with pkgs;
+              [
+                (python_.withPackages (
+                  ps: with ps; [
+                    setuptools
+                    wheel
+                    requests
+                    nanobind
+                  ]
+                ))
+                (operon_.overrideAttrs (old: {
+                  cmakeFlags = old.cmakeFlags ++ [ "-DUSE_SINGLE_PRECISION=1" ];
+                }))
+              ]
+              ++ operon_.buildInputs;
           };
         in
         rec {
@@ -66,13 +96,12 @@
             pyoperon-generic = pyoperon.overrideAttrs (old: {
               cmakeFlags = [
                 "-DCMAKE_BUILD_TYPE=Release"
-                "-DCMAKE_CXX_FLAGS=${
-                  if pkgs.hostPlatform.isx86_64 then "-march=x86-64" else ""
-                }"
+                "-DCMAKE_CXX_FLAGS=${if pkgs.hostPlatform.isx86_64 then "-march=x86-64" else ""}"
               ];
             });
-            pyoperon-debug = pyoperon.overrideAttrs
-              (old: { cmakeBuildType = "Debug"; });
+            pyoperon-debug = pyoperon.overrideAttrs (old: {
+              cmakeBuildType = "Debug";
+            });
           };
 
           devShells.default = stdenv_.mkDerivation {
@@ -84,8 +113,20 @@
           devShells.pyenv = stdenv_.mkDerivation {
             name = "pyoperon-dev";
             nativeBuildInputs = pyoperon.nativeBuildInputs;
-            buildInputs = pyoperon.buildInputs ++ (with pkgs; [ pdm virtualenv zlib ]) ++ (with python_.pkgs; [numpy pandas scikit-learn ]);
-              LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/:${pkgs.zlib}/lib/";
+            buildInputs =
+              pyoperon.buildInputs
+              ++ (with pkgs; [
+                pdm
+                virtualenv
+                zlib
+              ])
+              ++ (with python_.pkgs; [
+                numpy
+                pandas
+                scikit-learn
+                pytest
+              ]);
+            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/:${pkgs.zlib}/lib/";
           };
         };
     };
