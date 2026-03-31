26283bdcedfd626c4cda3dd0eafa0e9d09cccab5
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-03-31 18:01:20 +0300

    assorted fixes

--- ports/operon/portfile.cmake
+++ ports/operon/portfile.cmake
@@ -8,17 +8,12 @@ vcpkg_from_github(
         add-msvc-support.patch
 )
 
-include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake/vcpkg_cmake_build.cmake")
-include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake/vcpkg_cmake_install.cmake")
-include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake-config/vcpkg_cmake_config_fixup.cmake")
-
 set(VCPKG_BUILD_TYPE release)
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" OPERON_SHARED_LIBS)
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
-  PREFER_NINJA
   OPTIONS
         -DBUILD_TESTING=OFF
         -DBUILD_EXAMPLES=OFF
